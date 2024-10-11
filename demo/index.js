55f784a175b084f234f77f1b1368337d6526a86a
pc <1326278155@qq.com>
2024-10-11 12:45:35 +0800

    hello world

---
+++ demo/index.js
@@ -0,0 +1,10 @@
+// index.js
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+
+// 清除现有的 HTML 内容
+document.body.innerHTML = '<div id="app"></div>';
+
+// 渲染你的 React 组件
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+export default function MyApp(){
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+};
