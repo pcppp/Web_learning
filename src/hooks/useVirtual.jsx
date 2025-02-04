8b158eb22645a4a9b31cd2589fa65ed1a6dbf678
彭畅 <1326278155@qq.com>
2025-02-04 23:36:05 +0800

    feat:虚拟列表 part2

--- src/hooks/useVirtual.jsx
+++ src/hooks/useVirtual.jsx
@@ -1,41 +1,41 @@
 import { useEffect, useCallback, useMemo, useState } from 'react';
-export default useVirtual = ({ size, parentRef, estimateSize, overscan }) => {
-  const itemHeights = useMemo(
-    Array.from({ length: size }, (_, i) => estimateSize(i)),
-    [estimateSize, size]
-  );
+const useVirtual = ({ size, parentRef, estimateSize, overscan }) => {
+  const itemHeights = useMemo(() => Array.from({ length: size }, (_, i) => estimateSize(i)), [estimateSize, size]);
   const [startIndex, setStartIndex] = useState(0);
-  const endIndex = Math.min(startIndex + limit, list.length - 1);
-  const limit = () => {
+  const limit = (height) => {
     // 计算当前能够承下的元素数量
     let i = 0;
     let sum = 0;
-    while (sum < parentRef.current.height) {
+    while (sum < height) {
       sum += itemHeights[i];
       i++;
     }
     return i;
   };
-  const wraperHeight = useMemo(
-    itemHeights.reduce((sum, height) => sum + height, 0),
-    [itemHeights]
-  );
-  const renderList = () => {
+  const endIndex = Math.min(startIndex + limit, size - 1);
+  const wraperHeight = useMemo(() => itemHeights.reduce((sum, height) => sum + height, 0), [itemHeights]);
+  const renderList = (scrollTop) => {
     let arr = [];
+    let startHeight = scrollTop;
+    setStartIndex(limit(scrollTop));
     for (let i = Math.max(startIndex - overscan, 0); i < Math.min(size, endIndex + overscan); i++) {
-      arr.push();
+      if (i < startIndex) {
+      }
+      startHeight += itemHeights[i];
+      arr.push({ index: i, height: itemHeights[i], start: startHeight });
     }
   };
   const handleScroll = useCallback(() => {
-    if (listRef.current) {
-      const scrollTop = listRef.current.scrollTop;
-      setScrollPos(scrollTop);
-    }
+    const scrollTop = parentRef.current.scrollTop;
   }, [itemHeights]);
   useEffect(() => {
-    listRef.current.addEventListener('scroll', handleScroll);
+    if (parentRef.current) {
+      parentRef.current.addEventListener('scroll', handleScroll);
+    }
     return () => {
-      listRef.current.removeEventListener('scroll', handleScroll);
+      parentRef.current.removeEventListener('scroll', handleScroll);
     };
   }, [handleScroll]);
+  return { totalHeight: wraperHeight, virtualRows: {} };
 };
+export default useVirtual;
--- src/pages/virtualList/VirtualList.jsx
+++ src/pages/virtualList/VirtualList.jsx
@@ -1,4 +1,9 @@
 import ItemList from './components/ItemList';
+import useVirtual from '../../hooks/useVirtual';
+import { useRef, useState } from 'react';
 export default function VirtualList() {
-  return <ItemList></ItemList>;
+  const listRef = useRef();
+  const [items, setItems] = useState(['苹果', ...Array.from({ length: 100 }, () => '香蕉'), '橙子']);
+  const virtualizer = useVirtual({ size: items.length, estimateSize: () => 20, parentRef: listRef, overscan: 20 });
+  return <ItemList listRef={listRef} items={items}></ItemList>;
 }
