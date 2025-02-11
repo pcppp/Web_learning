31c3bc6e8c25a445ccb0a9892a9fcc2e7d4f92ef
彭畅 <1326278155@qq.com>
2025-02-11 12:04:27 +0800

    feat:虚拟列表初步实现

--- src/hooks/useVirtual.jsx
+++ src/hooks/useVirtual.jsx
@@ -1,51 +1,51 @@
-import { useEffect, useCallback, useMemo, useState } from 'react';
+import { useEffect, useCallback, useMemo, useState, useRef } from 'react';
 const useVirtual = ({ size, parentRef, estimateSize, overscan }) => {
   const itemHeights = useMemo(() => Array.from({ length: size }, (_, i) => estimateSize(i)), [estimateSize, size]);
   const [startIndex, setStartIndex] = useState(0);
+
   const [virtualRows, setVirtualRows] = useState([]);
-  const limit = (height) => {
+  const limit = (startIndex, height) => {
+    console.log('🚀 ~ limit ~ height:', height);
+
     // 计算当前能够承下的元素数量
-    let i = 0;
+    let i = startIndex;
     let sum = 0;
     while (sum < height) {
       sum += itemHeights[i];
       i++;
     }
+    console.log('🚀 ~ limit ~ i:', i);
+
     return i;
   };
   let endIndex = 0;
   if (parentRef.current) {
     endIndex = Math.min(startIndex + limit(parentRef.current.clientHeight), size - 1);
-    console.log('🚀 ~ useVirtual ~ startIndex:', startIndex);
-    console.log('🚀 ~ useVirtual ~ endIndex:', endIndex);
   }
   // 整个容器的高度
   const wraperHeight = useMemo(() => itemHeights.reduce((sum, height) => sum + height, 0), [itemHeights]);
 
-  const renderList = useCallback(
-    (scrollTop) => {
-      let negativeStartHeight = scrollTop;
-      let startHeight = scrollTop;
-      let listArr = [];
-      setStartIndex(limit(scrollTop));
-      for (let i = Math.max(startIndex - overscan, 0); i < Math.min(size, endIndex + overscan); i++) {
-        console.log('🚀 ~ useVirtual ~ endIndex + overscan:', endIndex + overscan);
-        if (i < startIndex) {
-          negativeStartHeight -= itemHeights[-(overscan - i)];
-          listArr.push({
-            index: -(overscan - i),
-            height: itemHeights[-(overscan - i)],
-            start: negativeStartHeight,
-          });
-        } else {
-          startHeight += itemHeights[i];
-          listArr.push({ index: i, height: itemHeights[i], start: startHeight });
-        }
+  const renderList = useCallback((scrollTop) => {
+    let negativeStartHeight = scrollTop;
+    let startHeight = scrollTop;
+    let renderStartIndex = limit(startIndex, scrollTop);
+    let listArr = [];
+    setStartIndex(renderStartIndex);
+    for (let i = Math.max(renderStartIndex - overscan, 0); i < Math.min(size, endIndex + overscan); i++) {
+      if (i < renderStartIndex) {
+        negativeStartHeight -= itemHeights[-(overscan - i)];
+        listArr.push({
+          index: -(overscan - i),
+          height: itemHeights[-(overscan - i)],
+          start: negativeStartHeight,
+        });
+      } else {
+        startHeight += itemHeights[i];
+        listArr.push({ index: i, height: itemHeights[i], start: startHeight });
       }
-      setVirtualRows(listArr);
-    },
-    [itemHeights]
-  );
+    }
+    setVirtualRows(listArr);
+  });
   const handleScroll = useCallback(() => {
     const scrollTop = parentRef.current.scrollTop;
     renderList(scrollTop);
